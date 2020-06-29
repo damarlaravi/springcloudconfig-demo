53a1874c0726278362e339c9efb9b829a77d404f
venkatang <venkatang1990@gmail.com>
2020-06-29 07:11:05 +0530

    first commit

---
+++ src/test/java/spring/cloud/demo/springcloudm2configserver/SpringCloudM2ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.cloud.demo.springcloudm2configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudM2ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
